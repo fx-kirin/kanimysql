3ea8aada2d8c29492584527c4b4768ef76c37941
Yoshiaki Ono <ono.kirin@gmail.com>
2018-12-06 14:09:54 +0900

    add table_dict_list

---
+++ kanimysql/cursor.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2018 zenbook <zenbook@zenbook-XPS>
+#
+# Distributed under terms of the MIT license.
+
+"""
+
+"""
+
+from pymysql.cursors import DictCursor
+from .core import TableDict
+
+dict_type = TableDict(None)
+class KaniCursor(DictCursor):
+    dict_type = dict_type
+    table_dict_list = {}
+    # You can override this to use OrderedDict or other dict-like types.
+    def setup_table_dict(self, table_dict_list):
+        self.table_dict_list = table_dict_list
+    
+    def _conv_row(self, row):
+        if row is None:
+            return None
+        table_name = self._result.description
+        if table_name in self.table_dict_list:
+            table_dict = self.table_dict_list[table_name]
+            return table_dict(zip(self._fields, row))
+        else:
+            return self.dict_type(zip(self._fields, row))
