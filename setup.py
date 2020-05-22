fb97002d9f282d14322736028465c74233b95b8d
fx_kirin <fx.kirin@gmail.com>
2020-05-22 16:50:48 +0900

    version up

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.md') as f:
     long_description = f.read()
 
 setup(name='kanimysql',
-      version='0.0.6',
+      version='0.1.0',
       description='A MySQL class for more convenient database manipulations with Python dictionary.',
       long_description=long_description,
       author='Kirin Fx',
